181df42ff97f3394864f789f115b1c89a9472bcf
Jon Bachelor <jon_bachelor@me.com>
2018-11-05 07:53:34 -0800

    Added NextPage to navigate to from MainPage, and implemented navigation.

--- LACodeCamp2018/LACodeCamp2018/ViewModels/MainPageViewModel.cs
+++ LACodeCamp2018/LACodeCamp2018/ViewModels/MainPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Commands;
+﻿using LACodeCamp2018.Views;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
 using System;
@@ -10,10 +11,19 @@ namespace LACodeCamp2018.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
+        public DelegateCommand NavToNextPageCommand { get; set; }
+
         public MainPageViewModel(INavigationService navigationService)
             : base(navigationService)
         {
             Title = "Main Page";
+            NavToNextPageCommand = new DelegateCommand(OnNavToNextPageTapped);
+        }
+
+        private async void OnNavToNextPageTapped()
+        {
+            System.Console.WriteLine($"{this.GetType().Name}.{nameof(OnNavToNextPageTapped)}");
+            await NavigationService.NavigateAsync(nameof(NextPage));
         }
     }
 }
--- LACodeCamp2018/LACodeCamp2018/ViewModels/NextPageViewModel.cs
+++ LACodeCamp2018/LACodeCamp2018/ViewModels/NextPageViewModel.cs
@@ -1,16 +1,16 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
+using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace LACodeCamp2018.ViewModels
 {
-	public class NextPageViewModel : BindableBase
+	public class NextPageViewModel : ViewModelBase
 	{
-        public NextPageViewModel()
+        public NextPageViewModel(INavigationService navigationService) : base(navigationService)
         {
-
         }
-	}
+    }
 }
