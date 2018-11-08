645816058b3c4a61ccb8c8c864bf695563e957e9
Jon Bachelor <jon_bachelor@me.com>
2018-11-07 17:29:06 -0800

    Adding alert so user can choose whether or not they really want to navigate to the NextPage.

--- LACodeCamp2018/LACodeCamp2018/ViewModels/MainPageViewModel.cs
+++ LACodeCamp2018/LACodeCamp2018/ViewModels/MainPageViewModel.cs
@@ -26,8 +26,19 @@ namespace LACodeCamp2018.ViewModels
 
         private async void OnNavToNextPageTapped()
         {
-            System.Console.WriteLine($"{this.GetType().Name}.{nameof(OnNavToNextPageTapped)}");
-            await NavigationService.NavigateAsync(nameof(NextPage));
+            Console.WriteLine($"{this.GetType().Name}.{nameof(OnNavToNextPageTapped)}");
+
+            bool userWouldLikeToNavigate = await _pageDialogService.DisplayAlertAsync("Really?",
+                "Are you sure you would like to navigate to the magical, mystical next page?",
+                "Yes!",
+                "Nope");
+
+            Console.WriteLine($"User has responded to the alert. Do they wish to navigate:  {userWouldLikeToNavigate}.");
+
+            if (userWouldLikeToNavigate)
+            {
+                await NavigationService.NavigateAsync(nameof(NextPage));
+            }
         }
     }
 }
