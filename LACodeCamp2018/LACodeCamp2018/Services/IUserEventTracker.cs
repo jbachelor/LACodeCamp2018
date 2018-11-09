77f4e763da63e891682076f84c75e1e872685cb3
Jon Bachelor <jon_bachelor@me.com>
2018-11-09 07:54:46 -0800

    Adding UserEventTracker service and interface.

---
+++ LACodeCamp2018/LACodeCamp2018/Services/IUserEventTracker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LACodeCamp2018.Services
+{
+    public interface IUserEventTracker
+    {
+        Dictionary<DateTime, string> GetEvents();
+        KeyValuePair<DateTime, string> GetLastEvent();
+        void TrackEvent(string userEvent);
+    }
+}
---
+++ LACodeCamp2018/LACodeCamp2018/Services/UserEventTracker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Prism.Events;
+
+namespace LACodeCamp2018.Services
+{
+    public class UserEventTracker : IUserEventTracker
+    {
+        IEventAggregator _eventAggregator;
+        Dictionary<DateTime, string> _trackedEventsDict;
+
+        public UserEventTracker(IEventAggregator eventAggregator)
+        {
+            Console.WriteLine($"{this.GetType().Name}:  ctor");
+            _trackedEventsDict = new Dictionary<DateTime, string>();
+            _eventAggregator = eventAggregator;
+        }
+
+        public void TrackEvent(string userEvent)
+        {
+            Console.WriteLine($"{this.GetType().Name}.{nameof(TrackEvent)}:  {userEvent}");
+            _trackedEventsDict.Add(DateTime.Now, userEvent);
+        }
+
+        public Dictionary<DateTime, string> GetEvents()
+        {
+            Console.WriteLine($"{this.GetType().Name}.{nameof(GetEvents)}");
+            return _trackedEventsDict;
+        }
+
+        public KeyValuePair<DateTime, string> GetLastEvent()
+        {
+            Console.WriteLine($"{this.GetType().Name}.{nameof(GetLastEvent)}");
+            return _trackedEventsDict.LastOrDefault();
+        }
+    }
+}
