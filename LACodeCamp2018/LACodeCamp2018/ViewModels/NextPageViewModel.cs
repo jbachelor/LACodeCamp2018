c901c7f0f5383839884dd55001725f64a1b8f5cc
Jon Bachelor <jon_bachelor@me.com>
2018-11-09 20:33:08 -0800

    Adding a property to store an ObservableCollection of keyValuePairs that come from the UserEventTracker in order to display in a listView.

--- LACodeCamp2018/LACodeCamp2018/ViewModels/NextPageViewModel.cs
+++ LACodeCamp2018/LACodeCamp2018/ViewModels/NextPageViewModel.cs
@@ -1,19 +1,39 @@
-﻿using Prism.Commands;
+﻿using LACodeCamp2018.Services;
+using Prism.Commands;
 using Prism.Events;
 using Prism.Mvvm;
 using Prism.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace LACodeCamp2018.ViewModels
 {
 	public class NextPageViewModel : ViewModelBase
 	{
+        protected IUserEventTracker _userEventTracker;
+
+        private ObservableCollection<KeyValuePair<DateTime, string>> _userEvents;
+        public ObservableCollection<KeyValuePair<DateTime, string>> UserEvents
+        {
+            get { return _userEvents; }
+            set { SetProperty(ref _userEvents, value); }
+        }
+
         public NextPageViewModel(INavigationService navigationService,
-            IEventAggregator eventAggregator) 
+            IEventAggregator eventAggregator,
+            IUserEventTracker userEventTracker)
             : base(navigationService, eventAggregator)
         {
+            _userEventTracker = userEventTracker;
+        }
+
+        public override void OnNavigatingTo(INavigationParameters parameters)
+        {
+            base.OnNavigatingTo(parameters);
+            Dictionary<DateTime, string> trackedEvents = _userEventTracker.GetEvents();
+            UserEvents = new ObservableCollection<KeyValuePair<DateTime, string>>(trackedEvents);
         }
     }
 }
